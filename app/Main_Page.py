f6091811995a01731a277783c38c52c822139b7f
0xrdt <0xdoing@gmail.com>
2022-08-06 00:30:43 -0300

    bugfixes, speed improvements and new features

--- app/Main_Page.py
+++ app/Main_Page.py
@@ -12,7 +12,7 @@ from utils.df_grid_builder import df_grid_builder
 
 st.set_page_config(
      page_title="The Wallet Analyzoooooor",
-     page_icon="🔎",
+     page_icon="🔍",
      layout="wide",
      initial_sidebar_state="expanded",
     #  menu_items={
@@ -20,6 +20,7 @@ st.set_page_config(
  )
 
 st.title('The Wallet Analyzoooooor')
+st.markdown('check our landing page: [analyzooor.notawizard.xyz](http://analyzooor.notawizard.xyz/)')
 
 st.info("Fetching the data may take some time. Please be patient. (it usually takes less than 1 minute)")
 
@@ -34,7 +35,7 @@ selected_sections = st.multiselect('Choose the sections you want to see:',
 											], 
 										   default=[])
 
-start_date = st.date_input(label='Start date', value=datetime.date(2022, 6, 1)).strftime('%Y-%m-%d')
+start_date = st.date_input(label='Start date', value=datetime.datetime.today()-datetime.timedelta(days=30)).strftime('%Y-%m-%d')
 wallet_address = st.text_input(label='Wallet address', value='0x41318419cfa25396b47a94896ffa2c77c6434040')
 rows_limit = st.number_input(label='Rows limit (useful if the app is crashing, but will break some charts)', 
 								value=100_000, min_value=1, max_value=100_000)
@@ -46,32 +47,50 @@ Some wallet ideas:
 - 0x581BEf12967f06f2eBfcabb7504fA61f0326CD9A: danner.eth (good for nfts)
 """)
 
-st.markdown("Wallet label:")
-wallet_label = load_wallet_label(wallet_address).copy()
 
+
+wallet_label = st.cache(load_wallet_label, ttl=60*60*12)(wallet_address).copy()
 if len(wallet_label) == 0:
-	st.write("No label found")
+	st.write("No wallet label found for the selected address")
 
 else:
+	st.markdown("Wallet label of the selected address:")
 	wallet_label = wallet_label[['ADDRESS_NAME', 'LABEL_TYPE', 'LABEL_SUBTYPE', 'LABEL']]
-	st.write(wallet_label)
+	st.table(wallet_label.set_index("ADDRESS_NAME"))
+
+if (('Transactions' in selected_sections) or ('Historical Balance' in selected_sections) or 
+   ('Transfers' in selected_sections) or ('NFTs!' in selected_sections)):
+
+	transactions_per_wallet, erc20_balances_per_wallet, native_token_transfers_per_wallet, \
+	token_transfers_per_wallet, nft_sales_df, nft_transfers_df = st.cache(load_df_bundle, ttl=60*60*12)(wallet_address, start_date, rows_limit)
+	
+	transactions_per_wallet, erc20_balances_per_wallet, native_token_transfers_per_wallet, \
+	token_transfers_per_wallet, nft_sales_df, nft_transfers_df = transactions_per_wallet.copy(), \
+	erc20_balances_per_wallet.copy(), native_token_transfers_per_wallet.copy(), \
+	token_transfers_per_wallet.copy(), nft_sales_df, nft_transfers_df.copy()
 
 if 'Transactions' in selected_sections:
 
 	st.markdown("## Transactions")
 	
-	transactions_per_wallet = load_transactions(start_date=start_date, wallet_address=wallet_address, rows_limit=rows_limit).copy()
 	if len(transactions_per_wallet)==0:
 		st.warning("No transactions found.")
 	else:
-		transactions_per_wallet['dummy']=10
-		fig = px.scatter(transactions_per_wallet, x="BLOCK_TIMESTAMP", y="LABEL", color="SIDE", size='dummy', size_max=10, opacity=0.5,
-			title='Labeled Transactions over time', hover_data=['TX_FEE', 'TX_HASH', 'ETH_VALUE'])
-		# fig.update_layout(xaxis_rangeslider_visible=True)
-		fig.update_layout(height=500)
-		fig.update_yaxes(title='Transaction per Label')
-		fig.update_xaxes(title='Date')
-		st.write(fig)
+
+		should_show_transactions_scatter = st.checkbox("Show scatter plot of transactions")
+		if should_show_transactions_scatter:
+
+			col_txs_1, col_txs_2, col_txs_3 = st.columns(3)
+			transactions_scatter_height = st.slider(label='Height of the transactions scatter plot', value=500, min_value=100, max_value=1000)
+
+			transactions_per_wallet['dummy']=10
+			fig = px.scatter(transactions_per_wallet, x="BLOCK_TIMESTAMP", y="LABEL", color="SIDE", size='dummy', size_max=10, opacity=0.5,
+				title='Labeled Transactions over time', hover_data=['TX_FEE', 'TX_HASH', 'ETH_VALUE'])
+			# fig.update_layout(xaxis_rangeslider_visible=True)
+			fig.update_layout(height=transactions_scatter_height)
+			fig.update_yaxes(title='Transaction per Label')
+			fig.update_xaxes(title='Date')
+			st.plotly_chart(fig, use_container_width=True)
 
 		should_agg_transactions = st.checkbox("Aggregate Transactions by day")
 		if should_agg_transactions:
@@ -86,7 +105,7 @@ if 'Transactions' in selected_sections:
 			fig.update_yaxes(title='Number of Transactions')
 			fig.update_xaxes(title='Date')
 			fig.update_layout(height=700)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 			agg_transactions_per_wallet = transactions_per_wallet\
 										.set_index('BLOCK_TIMESTAMP')\
@@ -99,7 +118,7 @@ if 'Transactions' in selected_sections:
 			fig.update_yaxes(title='Number of Transactions')
 			fig.update_xaxes(title='Date')
 			fig.update_layout(height=700)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 		should_show_raw_data = st.checkbox("Show transactions raw data")
 		if should_show_raw_data:
@@ -115,8 +134,6 @@ if 'Historical Balance' in selected_sections:
 
 	st.markdown("## Historical Balance")
 
-	erc20_balances_per_wallet = load_erc20_balances(start_date=start_date, wallet_address=wallet_address, rows_limit=rows_limit).copy()
-
 	if len(erc20_balances_per_wallet)==0:
 		st.warning("No ERC20 balances found.")
 	
@@ -125,7 +142,7 @@ if 'Historical Balance' in selected_sections:
 		fig.update_layout(title='Daily Balances')
 		fig.update_yaxes(title='Amount in USD')
 		fig.update_xaxes(title='Date')
-		st.write(fig)
+		st.plotly_chart(fig, use_container_width=True)
 
 
 		normalized_erc20_balances_per_wallet = erc20_balances_per_wallet.groupby(['BALANCE_DATE', 'SYMBOL'])['AMOUNT_USD'].sum()/erc20_balances_per_wallet.groupby(['BALANCE_DATE'])['AMOUNT_USD'].sum()
@@ -139,63 +156,79 @@ if 'Historical Balance' in selected_sections:
 		fig.update_layout(title='Normalized Daily Balances', height=700)
 		fig.update_yaxes(title='Normalized Amount in USD')
 		fig.update_xaxes(title='Date')
-		st.write(fig)
+		st.plotly_chart(fig, use_container_width=True)
 
-		st.markdown("Most recent balance:")
-		cols = ['BALANCE_DATE', 'SYMBOL', 'PRICE', 'BALANCE', 'AMOUNT_USD']
-		most_recent_balance_date = erc20_balances_per_wallet['BALANCE_DATE'].max()
-		most_recent_balance = erc20_balances_per_wallet[erc20_balances_per_wallet['BALANCE_DATE']==most_recent_balance_date]
-		df = most_recent_balance[cols].sort_values(by=['AMOUNT_USD'], ascending=False)
-		st.write(df)
+		should_show_most_recent_balance = st.checkbox("Show most recent balance")
+
+		if should_show_most_recent_balance:
+			st.markdown("Most recent balance:")
+			cols = ['BALANCE_DATE', 'SYMBOL', 'PRICE', 'BALANCE', 'AMOUNT_USD']
+			most_recent_balance_date = erc20_balances_per_wallet['BALANCE_DATE'].max()
+			most_recent_balance = erc20_balances_per_wallet[erc20_balances_per_wallet['BALANCE_DATE']==most_recent_balance_date]
+			df = most_recent_balance[cols].sort_values(by=['AMOUNT_USD'], ascending=False)
+			st.write(df)
 
 if 'Transfers' in selected_sections:
 
 	st.markdown("## Transfers")
 
-	native_token_transfers_per_wallet = load_native_token_transfers(start_date=start_date, wallet_address=wallet_address, rows_limit=rows_limit).copy()
-	token_transfers_per_wallet = load_erc20_token_transfers(start_date=start_date, wallet_address=wallet_address, rows_limit=rows_limit).copy()
-
 	if (len(native_token_transfers_per_wallet)==0) or (len(token_transfers_per_wallet)==0):
-		st.warning("No transfers found. (right now the account needs to have both ERC20 and ETH transfers, I'll fix it later")
+		st.warning("No transfers found. (right now the account needs to have both ERC20 and ETH transfers to show up here, I'll fix it later")
 	else:
 		agg_native_token_transfers_per_wallet = native_token_transfers_per_wallet.groupby(["LABEL", "SIDE"])[['AMOUNT_USD', 'AMOUNT']].sum()
 		agg_native_token_transfers_per_wallet = agg_native_token_transfers_per_wallet.reset_index()
 
-		fig = px.sunburst(agg_native_token_transfers_per_wallet, path=['SIDE', 'LABEL'], values='AMOUNT_USD', title='Total ETH Transfers')
-		fig.update_layout(height=700)
-		st.write(fig)
+		should_show_sunburst = st.checkbox("Show aggregate transfers", value=True)
+		if should_show_sunburst:
+			fig = px.sunburst(agg_native_token_transfers_per_wallet, path=['SIDE', 'LABEL'], values='AMOUNT_USD', title='Total ETH Transfers')
+			fig.update_layout(height=700)
+			st.plotly_chart(fig, use_container_width=True)
 
-		agg_token_transfers_per_wallet = token_transfers_per_wallet.groupby(["LABEL", "SIDE", 'SYMBOL'])[['AMOUNT_USD', 'AMOUNT']].sum()
-		agg_token_transfers_per_wallet = agg_token_transfers_per_wallet.reset_index()
+			agg_token_transfers_per_wallet = token_transfers_per_wallet.groupby(["LABEL", "SIDE", 'SYMBOL'])[['AMOUNT_USD', 'AMOUNT']].sum()
+			agg_token_transfers_per_wallet = agg_token_transfers_per_wallet.reset_index()
 
-		fig = px.sunburst(agg_token_transfers_per_wallet, path=['SIDE', 'SYMBOL', 'LABEL'], values='AMOUNT_USD', title='Total ERC-20 Transfers')
-		fig.update_layout(height=700)
-		st.write(fig)
+			fig = px.sunburst(agg_token_transfers_per_wallet, path=['SIDE', 'SYMBOL', 'LABEL'], values='AMOUNT_USD', title='Total ERC-20 Transfers')
+			fig.update_layout(height=700)
+			st.plotly_chart(fig, use_container_width=True)
 		
 		should_show_scatter_plot = st.checkbox("Show transfers scatter plot")
 		if should_show_scatter_plot:
 
+			col_eth_1, col_eth_2, col_eth_3 = st.columns(3)
+			eth_transfers_y = col_eth_1.selectbox("Select ETH transfers Y", ['LABEL', 'AMOUNT_USD', 'AMOUNT', 'SIDE'], index=0)
+			eth_transfers_color = col_eth_2.selectbox("Select ETH transfers color", ['LABEL', 'AMOUNT_USD', 'AMOUNT', 'SIDE'], index=3)
+			eth_transfers_facet_row = col_eth_3.selectbox("Select ETH transfers facet row", ['LABEL', 'SIDE', None], index=2)
+			eth_transfers_height = st.slider("Select ETH transfers height", min_value=200, max_value=2000, value=500, step=10)
+
 			native_token_transfers_per_wallet['dummy'] = 10
-			fig = px.scatter(native_token_transfers_per_wallet, x="BLOCK_TIMESTAMP", y="LABEL", color="SIDE", 
-						hover_data=['LABEL', 'AMOUNT', 'AMOUNT_USD'], size='dummy', size_max=10, opacity=0.5)
-			fig.update_layout(height=500, title='Labeled ETH Transfers over time')
-			fig.update_xaxes(title='Date')
-			fig.update_yaxes(title='Transfer per Label')
-			st.write(fig)
+			fig = px.scatter(native_token_transfers_per_wallet, x="BLOCK_TIMESTAMP", y=eth_transfers_y, color=eth_transfers_color, facet_row=eth_transfers_facet_row,
+						hover_data={'LABEL': True, 'AMOUNT': True, 'AMOUNT_USD': True, 'dummy': False, 'SIDE': True}, 
+						size='dummy', size_max=10, opacity=0.5)
+			fig.update_layout(height=eth_transfers_height, title='Labeled ETH Transfers over time')
+			# fig.update_xaxes(title='Date')
+			# fig.update_yaxes(title='Transfer per Label')
+			st.plotly_chart(fig, use_container_width=True)
+
+			col_erc20_1, col_erc20_2, col_erc20_3 = st.columns(3)
+			erc20_transfers_y = col_erc20_1.selectbox("Select ERC-20 transfers Y", ['LABEL', 'AMOUNT_USD', 'AMOUNT', 'SIDE', 'SYMBOL'], index=4)
+			erc20_transfers_color = col_erc20_2.selectbox("Select ERC-20 transfers color", ['LABEL', 'AMOUNT_USD', 'AMOUNT', 'SIDE', 'SYMBOL'], index=0)
+			erc20_transfers_facet_row = col_erc20_3.selectbox("Select ERC-20 transfers facet row", ['LABEL', 'SIDE', None, 'SYMBOL'], index=1)
+			erc20_transfers_height = st.slider("Select ERC-20 transfers height", min_value=200, max_value=2000, value=500, step=10)
 
 			token_transfers_per_wallet['dummy'] = 10
-			fig = px.scatter(token_transfers_per_wallet, x="BLOCK_TIMESTAMP", y='SYMBOL',color="LABEL", facet_row="SIDE", 
-					hover_data=['LABEL', 'AMOUNT', 'AMOUNT_USD'], size='dummy', size_max=10, opacity=0.5)
-			fig.update_layout(height=500, title='Labeled ERC-20 Transfers over time')
-			fig.update_xaxes(title='Date')
-			fig.update_yaxes(title='Transfer per Label')
-			st.write(fig)
+			fig = px.scatter(token_transfers_per_wallet, x="BLOCK_TIMESTAMP", y=erc20_transfers_y,color=erc20_transfers_color, facet_row=erc20_transfers_facet_row, 
+					hover_data={'LABEL': True, 'AMOUNT': True, 'AMOUNT_USD': True, 'dummy': False, 'SIDE': True, 'SYMBOL': True}, 
+					size='dummy', size_max=10, opacity=0.5)
+			fig.update_layout(height=erc20_transfers_height, title='Labeled ERC-20 Transfers over time')
+			# fig.update_xaxes(title='Date', row=0, col=1)
+			# fig.update_yaxes(title='Transfer per Label')
+			st.plotly_chart(fig, use_container_width=True)
 		
 		should_show_raw_data = st.checkbox("Show transfers raw data")
 		if should_show_raw_data:
 			cols = [
 				'TX_HASH', 'SIDE', 'BLOCK_TIMESTAMP', 'ORIGIN_FROM_ADDRESS', 
-				'ORIGIN_TO_ADDRESS', 'AMOUNT', 'AMOUNT_USD', 'ADDRESS_NAME', 
+				'ORIGIN_TO_ADDRESS', 'ETH_FROM_ADDRESS', 'ETH_TO_ADDRESS','AMOUNT', 'AMOUNT_USD', 'ADDRESS_NAME', 
 				'LABEL_TYPE', 'LABEL_SUBTYPE', 'LABEL',
 			]
 			df = native_token_transfers_per_wallet[cols].sort_values(by=['BLOCK_TIMESTAMP'])
@@ -203,12 +236,11 @@ if 'Transfers' in selected_sections:
 		
 			cols = [
 				'TX_HASH', 'SIDE', 'SYMBOL', 'BLOCK_TIMESTAMP', 'ORIGIN_FROM_ADDRESS', 
-				'ORIGIN_TO_ADDRESS', 'AMOUNT', 'AMOUNT_USD', 'ADDRESS_NAME', 
+				'ORIGIN_TO_ADDRESS', 'FROM_ADDRESS', 'TO_ADDRESS','AMOUNT', 'AMOUNT_USD', 'ADDRESS_NAME', 
 				'LABEL_TYPE', 'LABEL_SUBTYPE', 'LABEL',
 			]
 			df = token_transfers_per_wallet[cols].sort_values(by=['BLOCK_TIMESTAMP'])
 			df_grid_builder(df)
-
 
 if 'Transactions on Other EVM Chains' in selected_sections:
 	
@@ -220,8 +252,8 @@ if 'Transactions on Other EVM Chains' in selected_sections:
 
 	other_chains = ['arbitrum', 'optimism', 'avalanche', 'bsc', 'polygon']
 	for chain in other_chains:
-		print(chain)
-		tmp_df = load_transactions(wallet_address.lower(), start_date, rows_limit=rows_limit, chain_name=chain).copy()
+		# print(chain)
+		tmp_df = st.cache(load_transactions, ttl=60*60*12)(wallet_address.lower(), start_date, rows_limit=rows_limit, chain_name=chain).copy()
 		tmp_df['chain'] = chain
 		list_of_dfs.append(tmp_df)
 
@@ -240,7 +272,7 @@ if 'Transactions on Other EVM Chains' in selected_sections:
 					title='Labeled Transactions over time', hover_data=['TX_FEE', 'TX_HASH', 'ETH_VALUE'], facet_row='chain')
 			# fig.update_layout(xaxis_rangeslider_visible=True)
 			fig.update_layout(height=500*transactions_per_wallet_other_chains['chain'].nunique())
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 		
 		should_show_cross_chain_raw_data = st.checkbox("Show cross chain raw data")
 		if should_show_cross_chain_raw_data:
@@ -256,58 +288,59 @@ if 'NFTs!' in selected_sections:
 
 	st.markdown("## NFTs!")
 
-	nft_sales_df = load_nft_sales(wallet_address.lower(), start_date, rows_limit=rows_limit).copy()
-	nft_transfers_df = load_nft_transfers(wallet_address.lower(), start_date, rows_limit=rows_limit).copy()
-
 	should_show_nft_scatter_plot = st.checkbox('Show nfts scatter plots')
 	if should_show_nft_scatter_plot:
 		if len(nft_sales_df) > 0:
+			st.subheader('NFT Secondary Market')
 			nft_sales_df['dummy'] = 10
 
 			fig = px.scatter(nft_sales_df, x='BLOCK_TIMESTAMP', y='PROJECT_NAME', color='SIDE', size='dummy', size_max=10, opacity=0.5,
 						title='NFT sales/buys by project', hover_data=['PLATFORM_NAME', 'PRICE_USD', 'PRICE', 'TOKENID', 'PROJECT_NAME', 'EVENT_TYPE'])
 			fig.update_layout(height=500)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 			fig = px.scatter(nft_sales_df, x='BLOCK_TIMESTAMP', y='PRICE_USD', facet_row='SIDE', color='PROJECT_NAME',size='dummy', size_max=10, opacity=0.5,
 									title='NFT sales/buys by USD Price', hover_data=['PLATFORM_NAME', 'PRICE_USD', 'PRICE', 'TOKENID', 'PROJECT_NAME', 'EVENT_TYPE'], marginal_y='box')
 			fig.update_layout(height=500)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 			nft_sales_df_agg = nft_sales_df.groupby(['PROJECT_NAME', 'SIDE']).sum()['PRICE_USD'].reset_index()
 			nft_sales_df_agg.columns = ['PROJECT_NAME', 'SIDE', 'VOLUME_USD']
-			fig = px.sunburst(nft_sales_df_agg, path=['SIDE', 'PROJECT_NAME'], values='VOLUME_USD', title='Aggregate NFT sales volume', color='PROJECT_NAME')
+			fig = px.sunburst(nft_sales_df_agg, path=['SIDE', 'PROJECT_NAME'], values='VOLUME_USD', title='Aggregate NFT sales volume')
 			fig.update_layout(height=700)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 			nft_sales_df_by_platform = nft_sales_df.groupby(['PLATFORM_NAME']).agg({"PRICE_USD": "sum", "PRICE": "sum", "TX_HASH": "count"}).reset_index()
 			nft_sales_df_by_platform.columns = ['PLATFORM_NAME', 'VOLUME_USD', 'VOLUME', 'COUNT']
 			fig = px.bar(nft_sales_df_by_platform, x='PLATFORM_NAME', y='COUNT', title='Most used NFT platforms', hover_data=['COUNT', 'VOLUME_USD', 'VOLUME'])
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 		else:
 			st.warning("No NFT sales found")
 		
 		if len(nft_transfers_df) > 0:
+			st.subheader('NFT Mints and Transfers')
+
 			nft_transfers_df['dummy'] = 10
 			fig = px.scatter(nft_transfers_df, x='BLOCK_TIMESTAMP', y='PROJECT_NAME', color='SIDE', size='dummy', size_max=10, opacity=0.5,
 						title='NFT transfers and mints by project', hover_data=['TOKENID', 'PROJECT_NAME', 'EVENT_TYPE'])
 			fig.update_layout(height=500)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 			fig = px.scatter(nft_transfers_df, x='BLOCK_TIMESTAMP', y='PROJECT_NAME', color='EVENT_TYPE', facet_row='SIDE',size='dummy', size_max=10, opacity=0.5,
 						title='NFT transfers and mints by project (detailed by event type)', hover_data=['TOKENID', 'PROJECT_NAME', 'EVENT_TYPE'])
 			fig.update_layout(height=500)		
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 		else:
 			st.warning("No NFT transfers found")
 
 		if len(nft_sales_df) > 0 and len(nft_transfers_df) > 0:
+			st.subheader('NFT Aggregate Data')
 			agg_nfts_df = pd.concat([nft_sales_df[['BLOCK_TIMESTAMP', 'SIDE', 'PROJECT_NAME']], nft_transfers_df[['BLOCK_TIMESTAMP', 'SIDE', 'PROJECT_NAME']]], ignore_index=True)
 			agg_agg_nfts = agg_nfts_df.groupby(['SIDE', 'PROJECT_NAME']).count().reset_index()
 			agg_agg_nfts.columns = ['SIDE', 'PROJECT_NAME', 'COUNT']
-			fig = px.sunburst(agg_agg_nfts, path=['SIDE', 'PROJECT_NAME'], values='COUNT', title='Aggregate count of NFT movements (sales, mints, transfers)', color='PROJECT_NAME')
+			fig = px.sunburst(agg_agg_nfts, path=['SIDE', 'PROJECT_NAME'], values='COUNT', title='Aggregate count of NFT movements (sales, mints, transfers)')
 			fig.update_layout(height=700)
-			st.write(fig)
+			st.plotly_chart(fig, use_container_width=True)
 
 	should_show_raw_nft_data = st.checkbox('Show nfts raw data')
 	if should_show_raw_nft_data:
